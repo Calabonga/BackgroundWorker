9919664155e6203f1ad46682a329e3e4cbaf077e
Sergei Calabonga <dev@calabonga.net>
2020-09-30 17:10:01 +0700

    redundant files removed

--- Calabonga.BackgroundWorker.Api/Calabonga.BackgroundWorker.Api.Web/start - Copy (2).cmd
+++
@@ -1,3 +0,0 @@
-dotnet run -c Release --launch-profile Development2 Calabonga.BackgroundWorker.Api.Web.csproj
-
-PAUSE
--- Calabonga.BackgroundWorker.Api/Calabonga.BackgroundWorker.Api.Web/start - Copy.cmd
+++
@@ -1,3 +0,0 @@
-dotnet run -c Release --launch-profile Development1 Calabonga.BackgroundWorker.Api.Web.csproj
-
-PAUSE
